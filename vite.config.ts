ec2f5b2a4b551410fa37e0625ac538e662cf6082
abtomal <abtomalmusic@gmail.com>
2024-11-19 12:24:57 +0100

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import rewriteAll from 'vite-plugin-rewrite-all';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), rewriteAll()],
+  server: {
+    open: true, // Apre automaticamente il browser
+  },
+});
